0db48c60339b45371fc181cc45e7faed4215d9e6
Dad <Dad>
2017-01-05 12:16:32 -0500

    Make Fire

--- src/org/workers/impl/survival_expert/MakeFire.java
+++ src/org/workers/impl/survival_expert/MakeFire.java
@@ -1,11 +1,17 @@
 package org.workers.impl.survival_expert;
 
+import java.util.Comparator;
+import java.util.List;
+
 import org.OrionTutorial;
+import org.osbot.rs07.api.map.Area;
+import org.osbot.rs07.api.map.Position;
 import org.workers.TutorialWorker;
+
+import viking.api.Timing;
 
 public class MakeFire extends TutorialWorker
 {
-
 	public MakeFire(OrionTutorial mission)
 	{
 		super(mission);
@@ -21,6 +27,40 @@ public class MakeFire extends TutorialWorker
 	public void work()
 	{
 		script.log(this, false, "Make fire");
+		Position closestFree = getClosestFree();
+		if(closestFree != null)
+		{
+			if(walking.walk(closestFree) && Timing.waitCondition(() -> myPosition().equals(closestFree), 4000))
+			{
+				if(itemUtils.itemOnItem("Tinderbox", "Logs") && Timing.waitCondition(() -> myPlayer().isAnimating(), 2500))
+					Timing.waitCondition(() -> !inventory.contains("Logs"), 6500);
+			}
+		}
+		
+	}
+	
+	private Position getClosestFree()
+	{
+		Area lightableArea = new Area(myPosition().translate(-5, 5), myPosition().translate(5, -5));
+		List<Position> tiles = lightableArea.getPositions();
+		tiles.sort(posComparator());
+		
+		for(Position p : tiles)
+			if(objects.get(p.getX(), p.getY()).isEmpty())
+				return p;
+		
+		return null;
+	}
+	
+	private Comparator<Position> posComparator()
+	{
+		return new Comparator<Position>()
+		{
+			public int compare(Position p1, Position p2)
+			{	
+				return myPosition().distance(p1) - myPosition().distance(p2);
+			}
+		};
 	}
 
 }
