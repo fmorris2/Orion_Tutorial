6666c3aae12e021b45e575dec670602185998fd2
Dad <Dad>
2017-01-12 14:45:07 -0500

    Fixed a variety of bugs

--- src/org/workers/impl/mining_instructor/ExitMiningInstructor.java
+++ src/org/workers/impl/mining_instructor/ExitMiningInstructor.java
@@ -1,13 +1,16 @@
 package org.workers.impl.mining_instructor;
 
 import org.OrionTutorial;
+import org.osbot.rs07.api.map.Position;
 import org.workers.TutorialWorker;
 
 import viking.api.Timing;
 
 public class ExitMiningInstructor extends TutorialWorker
 {
-
+	private static final Position GATE_POS = new Position(3092, 9502, 0);
+	private static final int DIST_THRESH = 2;
+	
 	public ExitMiningInstructor(OrionTutorial mission)
 	{
 		super(mission);
@@ -25,6 +28,8 @@ public class ExitMiningInstructor extends TutorialWorker
 		script.log(this, false, "Exit Mining Instructor");
 		if(iFact.clickObject("Open", "Gate", 25).execute())
 			Timing.waitCondition(() -> myPosition().getX() > 3094, 7500);
+		else if(myPosition().distance(GATE_POS) > DIST_THRESH)
+			walkUtils.walkTo(GATE_POS);
 	}
 
 }
--- src/org/workers/impl/mining_instructor/MiningInstructorDialogue.java
+++ src/org/workers/impl/mining_instructor/MiningInstructorDialogue.java
@@ -1,11 +1,14 @@
 package org.workers.impl.mining_instructor;
 
 import org.OrionTutorial;
+import org.osbot.rs07.api.map.Position;
 import org.workers.TutorialWorker;
 
 public class MiningInstructorDialogue extends TutorialWorker
 {
-
+	private static final Position INSTRUCTOR_POS = new Position(3082, 9504, 0);
+	private static final int DIST_THRESH = 8;
+	
 	public MiningInstructorDialogue(OrionTutorial mission)
 	{
 		super(mission);
@@ -25,7 +28,9 @@ public class MiningInstructorDialogue extends TutorialWorker
 		if(myPosition().getY() < 9000) //up ladder
 			iFact.clickObject("Climb-down", "Ladder", 10).execute();
 		
-		iFact.dialogue("Talk-to", "Mining Instructor", 25).execute();
+		if(!iFact.dialogue("Talk-to", "Mining Instructor", 25).execute()
+				&& myPosition().distance(INSTRUCTOR_POS) > DIST_THRESH)
+			walkUtils.walkTo(INSTRUCTOR_POS);
 	}
 
 }
